6f3dbad72d7549583dab061a77d215f54a7a4a86
Suji Falcon <suji.falcon@gmail.com>
2022-11-06 16:54:17 -0500

    modal unfinished but everything else working

--- drag-race-api/src/Components/Modal.jsx
+++ drag-race-api/src/Components/Modal.jsx
@@ -1,18 +1,16 @@
 import style from './Modal.module.css';
 // import { useEffect } from 'react';
 
-function Modal({ queenName, closeModal }) {
+function Modal({ queenName, closeModal, queenImg }) {
 	return (
 		<div className={style.modalContainer} onClick={closeModal}>
 			<div id={style.modal}>
 				<div className={style.modalGuts}>
-					<img id={style.splash} alt=''>
-						{queenName.image_url}
-					</img>
+					<img src={queenImg} id={style.splash} alt='' />
 					<h3 id={style.queenName}>{queenName.name}</h3>
 					<p id={style.quote}>Quote: {queenName.quote}</p>
-					<p id={style.tags}>{queenName.tags.join(`, `)}</p>
-					<p id={style.blurb}>{queenName.blurb}</p>
+					{/* <p id={style.tags}>{queenName.tags.join(`, `)}</p>
+					<p id={style.blurb}>{queenName.blurb}</p> */}
 				</div>
 			</div>
 		</div>
